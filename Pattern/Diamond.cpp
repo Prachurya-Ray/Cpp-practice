0e08fc42e052d91b031a7047e7aac38881574c39
Prachurya Ray <70370699+Prachurya-Ray@users.noreply.github.com>
2022-07-16 13:15:43 +0530

    Add files via upload

---
+++ Pattern/Diamond.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+inline void replace (int i, int j, int n)
+{
+    for(j=1;j<=n-i;j++)
+        cout<<" ";
+        
+    for(j=1;j<=2*i-1;j++)
+        cout<<"*";     
+    cout<<endl;
+}
+
+int main()
+{
+    int n=5,i,j;
+    for(i=1;i<=n;i++)
+        replace (i,j,n);
+    for(i=n-1;i>=1;i--)
+        replace (i,j,n);     
+}
