b0a06c41628f06123f8ab01cd01a977db0c7fe15
Prachurya Ray <70370699+Prachurya-Ray@users.noreply.github.com>
2022-07-20 23:57:05 +0530

    Add files via upload

---
+++ Pattern/Double Triangle.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n=5,i,j;
+    
+    for(i=n;i>=1;i--)
+    {
+        for(j=1;j<=n-i;j++)
+            cout<<" ";
+        
+        for(j=1;j<=i;j++)
+            cout<<"* ";
+        cout<<endl;
+    }
+    
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n-i;j++)
+            cout<<" ";
+        
+        for(j=1;j<=i;j++)
+            cout<<"* ";
+        cout<<endl;
+    }
+ 
+        
+}
